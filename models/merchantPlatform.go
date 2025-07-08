464093427b50ae883815bf2fa52d0f7b6912af56
leo <leo@gmail.com>
2025-07-08 17:53:19 +0500

    float64 改decimal

--- models/merchantPlatform.go
+++ models/merchantPlatform.go
@@ -1,17 +1,19 @@
 package models
 
+import "github.com/shopspring/decimal"
+
 const TableNameMerchantPlatform = "merchant_platform"
 
 // MerchantPlatform undefined
 type MerchantPlatform struct {
-	MerchantPlatformId int64   `json:"merchant_platform_id" gorm:"merchant_platform_id,primaryKey"`
-	MerchantId         int     `json:"merchant_id" gorm:"merchant_id"` // 商户id
-	PlatformId         int     `json:"platform_id" gorm:"platform_id"` // 平台id
-	Rate               float64 `json:"rate" gorm:"rate"`               // 费率
-	Status             int     `json:"status" gorm:"status"`           // 状态 1 启用 2禁用
-	CreatedAt          int64   `json:"created_at" gorm:"created_at"`
-	UpdatedAt          int64   `json:"updated_at" gorm:"updated_at"`
-	DeletedAt          int64   `json:"deleted_at" gorm:"deleted_at"`
+	MerchantPlatformId int64           `json:"merchant_platform_id" gorm:"merchant_platform_id,primaryKey"`
+	MerchantId         int             `json:"merchant_id" gorm:"merchant_id"` // 商户id
+	PlatformId         int             `json:"platform_id" gorm:"platform_id"` // 平台id
+	Rate               decimal.Decimal `json:"rate" gorm:"rate"`               // 费率
+	Status             int             `json:"status" gorm:"status"`           // 状态 1 启用 2禁用
+	CreatedAt          int64           `json:"created_at" gorm:"created_at"`
+	UpdatedAt          int64           `json:"updated_at" gorm:"updated_at"`
+	DeletedAt          int64           `json:"deleted_at" gorm:"deleted_at"`
 }
 
 // TableName 表名称
--- models/userBalance.go
+++ models/userBalance.go
@@ -2,21 +2,22 @@ package models
 
 import (
 	"github.com/og-game/glib/stores/gormx"
+	"github.com/shopspring/decimal"
 )
 
 const TableNameUserBalance = "user_balance"
 
 // UserBalance 用户游戏余额表
 type UserBalance struct {
-	BalanceId           int64   `json:"balance_id" gorm:"balance_id;primaryKey"`          // 余额记录主键ID
-	UserId              int64   `json:"user_id" gorm:"user_id"`                           // 用户ID
-	MerchantId          int64   `json:"merchant_id" gorm:"merchant_id"`                   // 商户ID
-	PlatformId          int64   `json:"platform_id" gorm:"platform_id"`                   // 平台ID
-	Balance             float64 `json:"balance" gorm:"balance"`                           // 游戏内余额(微)
-	FrozenBalance       float64 `json:"frozen_balance" gorm:"frozen_balance"`             // 游戏内冻结余额
-	TransferringBalance float64 `json:"transferring_balance" gorm:"transferring_balance"` // 转账中余额
-	Version             int     `json:"version"`
-	gormx.Model                 // 删除时间戳（软删除）
+	BalanceId           int64           `json:"balance_id" gorm:"balance_id;primaryKey"`          // 余额记录主键ID
+	UserId              int64           `json:"user_id" gorm:"user_id"`                           // 用户ID
+	MerchantId          int64           `json:"merchant_id" gorm:"merchant_id"`                   // 商户ID
+	PlatformId          int64           `json:"platform_id" gorm:"platform_id"`                   // 平台ID
+	Balance             decimal.Decimal `json:"balance" gorm:"balance"`                           // 游戏内余额(微)
+	FrozenBalance       decimal.Decimal `json:"frozen_balance" gorm:"frozen_balance"`             // 游戏内冻结余额
+	TransferringBalance decimal.Decimal `json:"transferring_balance" gorm:"transferring_balance"` // 转账中余额
+	Version             int             `json:"version"`
+	gormx.Model                         // 删除时间戳（软删除）
 }
 
 // TableName 表名称
