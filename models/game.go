c182984fb1b6bcc4e21545eefb49affa2042b030
leo <leo@gmail.com>
2025-07-15 14:08:37 +0800

    修改游戏表

--- models/game.go
+++ models/game.go
@@ -3,31 +3,35 @@ package models
 import (
 	"github.com/og-game/glib/stores/gormx"
 	"github.com/shopspring/decimal"
+	"gorm.io/datatypes"
 )
 
 const TableNameGame = "game"
 
 // Game 游戏表
 type Game struct {
-	GameId              int64           `json:"game_id" gorm:"game_id;primaryKey"`                  // 游戏id
-	ThreeId             string          `json:"three_id" gorm:"three_id"`                           // 三方游戏id
-	PlatformId          int64           `json:"platform_id" gorm:"platform_id"`                     // 平台id
-	Rate                decimal.Decimal `json:"rate" gorm:"rate"`                                   // 厂商费率
-	CategoryCode        string          `json:"category_code" gorm:"category_code"`                 // 分类code
-	GameName            string          `json:"game_name" gorm:"game_name"`                         // 游戏名称
-	Icon                string          `json:"icon" gorm:"icon"`                                   // 图标
-	ThumbCover          string          `json:"thumb_cover" gorm:"thumb_cover"`                     // 缩略图
-	HorizontalCover     string          `json:"horizontal_cover" gorm:"horizontal_cover"`           // 垂直封面
-	VerticalCover       string          `json:"vertical_cover" gorm:"vertical_cover"`               // 水平封面
-	ScreenType          int             `json:"screen_type" gorm:"screen_type"`                     // 横竖屏 1都支持 2 竖屏 3横屏
-	Weigh               int             `json:"weigh" gorm:"weigh"`                                 // 排序值
-	Status              int             `json:"status" gorm:"status"`                               // 状态 1启用 2禁用
-	PlatformGameStatus  int             `json:"platform_game_status" gorm:"platform_game_status"`   // 状态 1启用 2禁用
-	IsTrialPlay         int             `json:"is_trial_play" gorm:"is_trial_play"`                 // 是否允许试玩 1 是 2否
-	IsDelayWithdraw     int             `json:"is_delay_withdraw" gorm:"is_delay_withdraw"`         // 是否需要延迟提款
-	DelayWithdrawSecond int             `json:"delay_withdraw_second" gorm:"delay_withdraw_second"` // 延迟提款秒数
-	RebateRate          decimal.Decimal `json:"rebate_rate" gorm:"rebate_rate"`                     // 返奖率
-	Remark              string          `json:"remark" gorm:"remark"`                               // 备注
+	GameId              int64                       `json:"game_id" gorm:"game_id;primaryKey"`                  // 游戏id
+	ThreeId             string                      `json:"three_id" gorm:"three_id"`                           // 三方游戏id
+	PlatformId          int64                       `json:"platform_id" gorm:"platform_id"`                     // 平台id
+	Countries           datatypes.JSONSlice[string] `json:"countries" gorm:"countries"`                         // 国家列表
+	Languages           datatypes.JSONSlice[string] `json:"languages" gorm:"languages"`                         // 语言列表
+	Currencies          datatypes.JSONSlice[string] `json:"currencies" gorm:"currencies"`                       // 货币列表
+	Rate                decimal.Decimal             `json:"rate" gorm:"rate"`                                   // 厂商费率
+	CategoryCode        string                      `json:"category_code" gorm:"category_code"`                 // 分类code
+	GameName            string                      `json:"game_name" gorm:"game_name"`                         // 游戏名称
+	Icon                string                      `json:"icon" gorm:"icon"`                                   // 图标
+	ThumbCover          string                      `json:"thumb_cover" gorm:"thumb_cover"`                     // 缩略图
+	HorizontalCover     string                      `json:"horizontal_cover" gorm:"horizontal_cover"`           // 垂直封面
+	VerticalCover       string                      `json:"vertical_cover" gorm:"vertical_cover"`               // 水平封面
+	ScreenType          int                         `json:"screen_type" gorm:"screen_type"`                     // 横竖屏 1都支持 2 竖屏 3横屏
+	Weigh               int                         `json:"weigh" gorm:"weigh"`                                 // 排序值
+	Status              int                         `json:"status" gorm:"status"`                               // 状态 1启用 2禁用
+	PlatformGameStatus  int                         `json:"platform_game_status" gorm:"platform_game_status"`   // 状态 1启用 2禁用
+	IsTrialPlay         int                         `json:"is_trial_play" gorm:"is_trial_play"`                 // 是否允许试玩 1 是 2否
+	IsDelayWithdraw     int                         `json:"is_delay_withdraw" gorm:"is_delay_withdraw"`         // 是否需要延迟提款
+	DelayWithdrawSecond int                         `json:"delay_withdraw_second" gorm:"delay_withdraw_second"` // 延迟提款秒数
+	RebateRate          decimal.Decimal             `json:"rebate_rate" gorm:"rebate_rate"`                     // 返奖率
+	Remark              string                      `json:"remark" gorm:"remark"`                               // 备注
 	gormx.Model
 }
 
--- models/gamePlatform.go
+++ models/gamePlatform.go
@@ -14,6 +14,7 @@ type GamePlatform struct {
 	PlatformCode string                      `json:"platform_code" gorm:"platform_code"`        // 平台code
 	Countries    datatypes.JSONSlice[string] `json:"countries" gorm:"countries"`                // 国家列表
 	Languages    datatypes.JSONSlice[string] `json:"languages" gorm:"languages"`                // 语言列表
+	Currencies   datatypes.JSONSlice[string] `json:"currencies" gorm:"currencies"`              // 货币列表
 	Extend       datatypes.JSON              `json:"extend" gorm:"extend"`
 	WalletType   int                         `json:"wallet_type" gorm:"wallet_type"` // 钱包类型 1 单一  2转账
 	Weigh        int                         `json:"weigh" gorm:"weigh"`             // 排序值
