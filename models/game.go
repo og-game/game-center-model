f4690a24741dcc1c434288ba2450037a402aceed
leo <leo@gmail.com>
2025-07-15 11:52:56 +0800

    修改游戏表

--- models/game.go
+++ models/game.go
@@ -12,7 +12,7 @@ type Game struct {
 	GameId              int64           `json:"game_id" gorm:"game_id;primaryKey"`                  // 游戏id
 	ThreeId             string          `json:"three_id" gorm:"three_id"`                           // 三方游戏id
 	PlatformId          int64           `json:"platform_id" gorm:"platform_id"`                     // 平台id
-	PlatformRate        decimal.Decimal `json:"platform_rate" gorm:"platform_rate"`                 // 厂商费率
+	Rate                decimal.Decimal `json:"rate" gorm:"rate"`                                   // 厂商费率
 	CategoryCode        string          `json:"category_code" gorm:"category_code"`                 // 分类code
 	GameName            string          `json:"game_name" gorm:"game_name"`                         // 游戏名称
 	Icon                string          `json:"icon" gorm:"icon"`                                   // 图标
@@ -22,6 +22,7 @@ type Game struct {
 	ScreenType          int             `json:"screen_type" gorm:"screen_type"`                     // 横竖屏 1都支持 2 竖屏 3横屏
 	Weigh               int             `json:"weigh" gorm:"weigh"`                                 // 排序值
 	Status              int             `json:"status" gorm:"status"`                               // 状态 1启用 2禁用
+	PlatformGameStatus  int             `json:"platform_game_status" gorm:"platform_game_status"`   // 状态 1启用 2禁用
 	IsTrialPlay         int             `json:"is_trial_play" gorm:"is_trial_play"`                 // 是否允许试玩 1 是 2否
 	IsDelayWithdraw     int             `json:"is_delay_withdraw" gorm:"is_delay_withdraw"`         // 是否需要延迟提款
 	DelayWithdrawSecond int             `json:"delay_withdraw_second" gorm:"delay_withdraw_second"` // 延迟提款秒数
