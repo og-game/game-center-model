b4ef657226b37de03e8315e439667627dae2cdb1
leo <leo@gmail.com>
2025-07-22 10:47:25 +0800

    修改厂商表 商户游戏表

--- models/merchantGame.go
+++ models/merchantGame.go
@@ -1,15 +1,21 @@
 package models
 
-import "github.com/og-game/glib/stores/gormx"
+import (
+	"github.com/og-game/glib/stores/gormx"
+	"github.com/shopspring/decimal"
+	"gorm.io/datatypes"
+)
 
 const TableNameMerchantGame = "merchant_game"
 
 // MerchantGame undefined
 type MerchantGame struct {
-	MerchantGameId int64 `json:"merchant_game_id" gorm:"merchant_game_id"`
-	MerchantId     int64 `json:"merchant_id" gorm:"merchant_id"` // 商户id
-	GameId         int64 `json:"game_id" gorm:"game_id"`         // 游戏id
-	Status         int   `json:"status" gorm:"status"`           // 状态 1 启用 2禁用
+	MerchantGameId int64                       `json:"merchant_game_id" gorm:"merchant_game_id"`
+	MerchantId     int64                       `json:"merchant_id" gorm:"merchant_id"` // 商户id
+	GameId         int64                       `json:"game_id" gorm:"game_id"`         // 游戏id
+	Images         datatypes.JSONSlice[string] `json:"images" gorm:"images"`           // 图片列表
+	Rate           decimal.Decimal             `json:"rate" gorm:"rate"`               // 费率
+	Status         int                         `json:"status" gorm:"status"`           // 状态 1 启用 2禁用
 	gormx.Model
 }
 
