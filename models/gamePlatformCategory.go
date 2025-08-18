6125767413f1d89dcf30558548c6a7e3746b0d77
leo <leo@gmail.com>
2025-08-18 10:20:20 +0800

    新增游戏厂商分类关系映射表

--- models/gamePlatformCategory.go
+++ models/gamePlatformCategory.go
@@ -4,7 +4,7 @@ const TableNameGamePlatformCategory = "game_platform_category"
 
 // GamePlatformCategory 厂商游戏类型code映射表
 type GamePlatformCategory struct {
-	GamePlatformLanguageId int64  `json:"game_platform_language_id" gorm:"game_platform_language_id"`
+	GamePlatformCategoryId int64  `json:"game_platform_category_id" gorm:"game_platform_category_id"`
 	PlatformId             int64  `json:"platform_id" gorm:"platform_id"`                       // 游戏厂商id
 	CategoryCode           string `json:"category_code" gorm:"category_code"`                   // 类型code
 	PlatformCategoryCode   string `json:"platform_category_code" gorm:"platform_category_code"` // 厂商类型code
