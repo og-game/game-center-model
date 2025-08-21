ccc6c46cae13f1c935699ac59d91cbca07bc1521
s11age <o1was@ogmail.org>
2025-08-21 11:46:01 +0800

    Update gameBetSummary.go

--- models/gameBetSummary.go
+++ models/gameBetSummary.go
@@ -18,6 +18,7 @@ type GameBetSummary struct {
 	GameID              int64           `json:"game_id" grom:"game_id"`                             // 游戏ID
 	OriginalPlatformID  int64           `json:"original_platform_id" gorm:"original_platform_id"`   // 原始厂商id
 	OriginalGameID      int64           `json:"original_game_id" gorm:"original_game_id"`           // 原始游戏id
+	IsDiversion         int8            `json:"is_diversion" gorm:"is_diversion"`                   // 是否切流：1-开启切流，2-不开启切流
 	CategoryCode        string          `json:"category_code" gorm:"category_code"`                 // 分类code
 	CurrencyCode        string          `json:"currency_code" grom:"currency_code"`                 // 币种code
 	BetCount            int64           `json:"bet_count" grom:"bet_count"`                         // 有效投注笔数（不含取消）
