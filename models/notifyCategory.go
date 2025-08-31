09c756dc9aaa4dd0ffd704214e13072f1bc5de4b
s11age <o1was@ogmail.org>
2025-08-31 11:31:25 +0800

    Update notifyCategory.go

--- models/notifyCategory.go
+++ models/notifyCategory.go
@@ -12,8 +12,6 @@ type NotifyCategory struct {
 	CategoryCode     string `json:"category_code" gorm:"category_code;uniqueIndex:uniq_category_code;type:varchar(64);not null;comment:大类代码（唯一标识）---保持和proto文件一致"`
 	Name             string `json:"name" gorm:"name;type:varchar(64);not null;comment:大类名称"`
 	Description      string `json:"description" gorm:"description;type:varchar(255);comment:大类描述"`
-	AllowBatch       uint8  `json:"allow_batch" gorm:"allow_batch;type:tinyint unsigned;not null;default:1;comment:是否允许批量推送 1=是 2=否"`
-	DefaultBatchSize uint   `json:"default_batch_size" gorm:"default_batch_size;type:int unsigned;not null;default:10;comment:默认批量大小"`
 	gormx.Model
 }
 
