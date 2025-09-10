9a5c79f243b591eb98b70c6e96670267e6902a4c
s11age <o1was@ogmail.org>
2025-09-10 12:08:43 +0800

    Update riskAssociationMonitor.go

--- models/riskAssociationMonitor.go
+++ models/riskAssociationMonitor.go
@@ -13,8 +13,8 @@ type RiskAssociationMonitor struct {
 	MerchantID        int    `gorm:"column:merchant_id;not null;index:idx_merchant_id" json:"merchant_id"`
 	MerchantName      string `gorm:"column:merchant_name;type:varchar(100)" json:"merchant_name"`
 	CurrencyCode      string `gorm:"column:currency_code;type:varchar(10)" json:"currency_code"`
-	AssociationReason string `gorm:"column:association_reason;type:varchar(255)" json:"association_reason"`
-	AssociationInfo   string `gorm:"column:association_info;type:text" json:"association_info"`
+	AssociationType   int8   `gorm:"column:association_type" json:"association_type"`
+	AssociationInfo   string `gorm:"column:association_info;type:varchar(255)" json:"association_info"`
 	AssociationCount  int    `gorm:"column:association_count;default:0;index:idx_association_count" json:"association_count"`
 	Status            int8   `gorm:"column:status;default:1" json:"status"` // 1-有效，2-无效
 
