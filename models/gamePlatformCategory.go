896a1bbf8e804e16d8a48bacc1e2f917d93be3c0
ohwin <ohwin@ohwins-Mac-mini.local>
2026-05-14 18:15:57 +0800

    字段定义修改

--- models/gamePlatformCategory.go
+++ models/gamePlatformCategory.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/og-game/glib/stores/gormx"
+
 const TableNameGamePlatformCategory = "game_platform_category"
 
 // GamePlatformCategory 厂商游戏类型code映射表
@@ -9,9 +11,7 @@ type GamePlatformCategory struct {
 	CategoryCode           string `json:"category_code" gorm:"category_code"`                   // 类型code
 	PlatformCategoryCode   string `json:"platform_category_code" gorm:"platform_category_code"` // 厂商类型code
 	Status                 int    `json:"status" gorm:"status"`                                 // 状态 1 启用 2禁用
-	CreatedAt              int64  `json:"created_at" gorm:"created_at"`
-	UpdatedAt              int64  `json:"updated_at" gorm:"updated_at"`
-	DeletedAt              int64  `json:"deleted_at" gorm:"deleted_at"`
+	gormx.Model
 }
 
 // TableName 表名称
